2c35facbd94555805608ce1fe6571ffb7cd2862e
Grafcube <34889224+Grafcube@users.noreply.github.com>
2021-09-13 11:43:28 +0530

    Update install script to new extension

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ restart_nautilus()
     read -p "Do you want restart Nautilus (Files) [Y/N]? " VAR
     if [[ $VAR = 'y' || $VAR = 'Y' ]]; then
         nautilus -q
-    fi    
+    fi
 }
 cp_file() {
-    FILE=vscode-nautilus.py
+    FILE=vscodium-nautilus.py
     TARGDIR=$1
     if [[ ! -d $TARGDIR ]]; then
         mkdir -v -p "$TARGDIR"
@@ -18,7 +18,7 @@ cp_file() {
     restart_nautilus
 }
 cp_file_sudo() {
-    FILE=vscode-nautilus.py
+    FILE=vscodium-nautilus.py
     TARGDIR=$1
     if [[ ! -d $TARGDIR ]]; then
         sudo mkdir -v -p "$TARGDIR"
@@ -29,12 +29,12 @@ cp_file_sudo() {
 
 if [[ $UID != 0 ]]; then
     read -p "This script is running without sudo, install for current user [Y/N]? " VAR
-    if [[ $VAR = 'y' || $VAR = 'Y' ]]; then 
+    if [[ $VAR = 'y' || $VAR = 'Y' ]]; then
         TARGDIR=~/.local/share/nautilus-python/extensions
         cp_file "$TARGDIR"
     else
         read -p "Do you want install for all user [Y/N]? " VAR
-        if [[ $VAR = 'y' || $VAR = 'Y' ]]; then 
+        if [[ $VAR = 'y' || $VAR = 'Y' ]]; then
             TARGDIR=/usr/share/nautilus-python/extensions
             cp_file_sudo "$TARGDIR"
         else
@@ -50,4 +50,3 @@ if [[ $UID != 0 ]]; then
             echo "Install failed!"
         fi
 fi
-
